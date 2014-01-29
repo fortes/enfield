d9a11d5894d469ebb5b9dee6bbdd969cab95f253
Filipe Fortes <accounts@fortes.com>
2014-01-29 10:13:23 -0800

    Remove global CoffeeScript requirement. Upgrade CoffeeScript to 1.7. Fixes #11

--- bin/enfield.coffee
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env coffee
-
-require('../src/enfield').main process.argv
---
+++ bin/enfield.js
@@ -0,0 +1,5 @@
+#!/usr/bin/env node
+// Enable auto-compilation via require
+require("coffee-script/register");
+// Launch Enfield
+require('../src/enfield').main(process.argv)
